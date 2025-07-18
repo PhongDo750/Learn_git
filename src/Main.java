eb9cd180ef5b39ee7ad3438f417583ce8ccdbf81
PhongDo750 <dogiaphong2k4@gmail.com>
2025-07-18 15:00:38 +0700

    phong

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello");
-        System.out.println();
         System.out.println("Phong");
         System.out.println("Phong-2");
+        System.out.println("Phong-1");
     }
 }
